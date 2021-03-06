c7cded436f627d9eb2fdb80c209ec77d2eefa2e8
Charles Curi <charles.curi@gmail.com>
2021-03-06 12:01:34 -0300

    método de podt para inserção

--- src/main/java/mystore/controller/ProductsController.java
+++ src/main/java/mystore/controller/ProductsController.java
@@ -2,10 +2,15 @@ package mystore.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import mystore.model.Products;
 import mystore.repository.ProductsRepository;
 
 /**
@@ -14,7 +19,8 @@ import mystore.repository.ProductsRepository;
  * a anotação de origem é para testes em localhost
  */
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = {"http://localhost:4200","http://localhost:4200/add"})
+
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
@@ -29,5 +35,10 @@ public class ProductsController {
 	 public List findAll(){
 	    return repository.findAll();
 	 }
+	 
+	 @PostMapping
+	    public Products createProduct( @RequestBody Products product) {
+	        return repository.save(product);
+	    }
 
 }
